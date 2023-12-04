a053b009db9c59d8886f2b6b721fec95c233e89e
Edward Diaz <diazedward24@gmail.com>
2023-12-04 13:23:58 -0800

    Refactor code for readability. Refactor with conditional. Add refresh button

--- vite-project/catch_joke.js
+++ vite-project/catch_joke.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const displayJoke = document.getElementById("display-joke");
+const displayQuote = document.getElementById("display-quote");
+const displayCharacter = document.getElementById("display-character");
+const refreshButton = document.getElementById("refresh-button");
 
 export async function fetchData() {
     const errorMessage = `Mistakes were made. Try Again!`;
@@ -9,13 +11,32 @@ export async function fetchData() {
 
     try {
         const response = await axios.get(apiURL);
-        
-        const randomQuote = response.data.quote;
-        displayJoke.textContent = randomQuote;
-        console.log(randomQuote);
+
+        if (Array.isArray(response.data) && response.data.length > 0) {
+            const randomQuoteIndex = Math.floor(Math.random() * response.data.length);
+            const randomQuoteObject = response.data[randomQuoteIndex];
+
+            const randomQuote = randomQuoteObject.quote;
+            const randomCharacter = randomQuoteObject.character;
+
+            displayQuote.textContent = `"${randomQuote}"`;
+            displayCharacter.textContent = `- ${randomCharacter}`;
+
+            console.log(randomQuote);
+            console.log(randomCharacter);
+
+        } else {
+            throw new Error("No quotes found in the response.");
+        }
 
     } catch (error) {
-        displayJoke.textContent = errorMessage;
+        displayQuote.textContent = errorMessage;
         console.error(error);
     }
 }
+
+const refreshPage = () => {
+    location.reload();
+  }
+  
+refreshButton.addEventListener('click', refreshPage);
